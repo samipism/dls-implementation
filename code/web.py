b3b109944b084233d4254fdc681c99f35840f84c
Samip Poudel <samipism@Samips-MacBook-Air.local>
2024-11-15 01:23:58 +0530

    Web py added

--- code/web.py
+++ code/web.py
@@ -3,8 +3,10 @@ from inference import inference_ODI
 
 target_score_dls = 0
 target_score_dl_pro = 0
-Z_dls_params = [ 1.471e+00 , 5.794e-01 , 4.691e-02 ,-1.326e+00]
-Z_std_params = [ 2.609e+02,  1.204e-02, -1.252e-01,  2.624e-03,  1.758e-04]
+Z_dls_params = [3.216e-01 , -4.633e+00,  3.518e-02 , -2.623e-01]
+Z_std_params = [3.216e-01 , -4.633e+00,  3.518e-02 , -2.623e-01]
+# Z_dls_params = [ 1.471e+00 , 5.794e-01 , 4.691e-02 ,-1.326e+00]
+# Z_std_params = [ 2.609e+02,  1.204e-02, -1.252e-01,  2.624e-03,  1.758e-04]
 
 with st.sidebar:
     st.title("Cricket Target Score Prediction")
@@ -14,7 +16,7 @@ with st.sidebar:
     team2_overs_down = st.number_input("Team 2 Overs Down", value=20)
     lost_overs_due_to_rain = st.number_input("Lost Overs Due to Rain", value=5)
     if st.button("Calculate Target"):
-        target_score_dls = inference_ODI(Z_dls_params,Z_std_params, team1_score, team2_wicktes_down, team2_overs_down, lost_overs_due_to_rain)
+        target_score_dls = inference_ODI(Z_dls_params,Z_std_params, team1_score, team2_wicktes_down, team2_overs_down, lost_overs_due_to_rain, method='DLS')
 
 st.write('''
          # Duckworth Lewis Stern Method
